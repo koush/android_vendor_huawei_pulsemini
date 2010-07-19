a20030a222d514ec967ed550f3961e9b99326374
Koushik Dutta <koushd@gmail.com>
2010-07-18 23:10:36 -0700

    cleanup and fix comandline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,34 +7,10 @@ TARGET_CPU_ABI := armeabi
 
 TARGET_NO_BOOTLOADER := true
 
-# Wifi related defines
-BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := libWifiApi
-BOARD_WLAN_TI_STA_DK_ROOT   := system/wlan/ti/sta_dk_4_0_4_32
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_ARG      := ""
-WIFI_DRIVER_MODULE_NAME     := "wlan"
-WIFI_FIRMWARE_LOADER        := "wlan_loader"
-
 TARGET_BOOTLOADER_BOARD_NAME := MSM7x25
 
-BOARD_KERNEL_CMDLINE := BOARD_KERNEL_CMDLINE mem=211M console=ttyMSM2,115200n8 androidboot.hardware=qcom console=ttyUSBCONSOLE0 androidboot.console=ttyUSBCONSOLE0
+BOARD_KERNEL_CMDLINE := mem=211M console=ttyMSM2,115200n8 androidboot.hardware=qcom console=ttyUSBCONSOLE0 androidboot.console=ttyUSBCONSOLE0
 BOARD_KERNEL_BASE := 0x00200000
-
-BOARD_HAVE_BLUETOOTH := true
-
-TARGET_HARDWARE_3D := false
-
-BOARD_GPS_LIBRARIES := libgps librpc
-
-BOARD_VENDOR_USE_AKMD := true
-
-USE_PV_WINDOWS_MEDIA := false
-
-BOARD_USE_HTC_APPS := true
-
-BOARD_USE_HTC_LIBSENSORS := true
-BOARD_HTC_LIBSENSORS_NAME := sensors.sapphire
 
 BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00280000)
 BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
@@ -42,5 +18,3 @@ BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x05a00000)
 BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x127c0000)
 # The size of a block that can be marked bad.
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-USE_CAMERA_STUB := true
